8be2def4106ec57f44f2ee6a438b10b6260f84e1
TAMOM-dev <tamom.dev.git@outlook.com>
2025-03-28 16:45:00 -0300

    test commit

---
+++ src/test/java/dev/cliniq/cliniq/CliniqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cliniq.cliniq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
